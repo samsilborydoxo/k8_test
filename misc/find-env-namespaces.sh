4d66bcd4ee2c9331c748ca6a8c4bac60cf50d5ba
silborynirtmata <sam@nirmata.com>
2019-09-27 10:32:16 -0700

    switch ordering of nev and ns

--- misc/find-env-namespaces.sh
+++ misc/find-env-namespaces.sh
@@ -8,11 +8,11 @@ for namespace in $(kubectl get namespace --no-headers |awk '{print $1}'); do
         #echo found pod $pod $namespace
         nirmata_env=$(kubectl -n $namespace get pod $pod --show-labels --no-headers |awk '{print $6}' |grep -oP 'nirmata.io/environment.name=.*,' |sed -e 's,nirmata.io/environment.name=,,' -e s/,//)
         if [ ! -z $nirmata_env ];then
-            echo $namespace:$nirmata_env >>/tmp/find-env-namespaces-tmp.txt
+            echo $nirmata_env:$namespace >>/tmp/find-env-namespaces-tmp.txt
         fi
     done
 done
 
-echo Namespace:Env
+echo Env:Namespace
 sort /tmp/find-env-namespaces-tmp.txt  |uniq
 rm -f /tmp/find-env-namespaces-tmp.txt
