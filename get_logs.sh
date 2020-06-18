df140d9fee0269a98cef486c78b92168cdfede5e
Samuel Silbory <sam@nirmata.com>
2020-06-18 12:38:14 -0700

    get logs for all containers, cleanup help, sclean up shell check with quotes

--- get_logs.sh
+++ get_logs.sh
@@ -6,7 +6,7 @@ taillines=50000
 datastamp=$(date "+%Y%m%d-%H%M%S")
 startdir=$(pwd)
 
-while getopts 't:p:n:' OPTION; do
+while getopts 't:p:n:h' OPTION; do
   case "$OPTION" in
     p)
       pod="$OPTARG"
@@ -17,8 +17,12 @@ while getopts 't:p:n:' OPTION; do
     t)
       taillines="$OPTARG"
       ;;
+    h)
+      echo "script usage: $(basename "$0") [-n namespace] [-t number_of_log_lines] [ -p pod_name ]" >&2
+      exit 0
+      ;;
     ?)
-      echo "script usage: $(basename $0) [-n namespace] [-t number_of_log_lines] [ -p pod_name ]" >&2
+        echo "script usage: $(basename "$0") [-n namespace] [-t number_of_log_lines] [ -p match_string_4_pods ]" >&2
       exit 1
       ;;
   esac
@@ -28,21 +32,25 @@ shift "$(($OPTIND -1))"
 echo "namespace is $namespace"
 echo "pod match string is $pod"
 
-running_pods=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" -n $namespace  |grep $pod)
-echo Found runing pods $running_pods
+running_pods=$(kubectl get pods --no-headers -o custom-columns=":metadata.name" -n "$namespace"  |grep "$pod")
+if [ -z "$running_pods" ]; then 
+    echo "No pods found for $pod in  $namespace"
+    exit 0
+fi
+echo "Found runing pods: $running_pods"
 echo 
 
-rm -rf /tmp/k8-logs-script-$namespace-$datastamp
-if [ -e /tmp/k8-logs-script-$namespace-$datastamp ];then echo /tmp/k8-logs-script-$namespace-$datastamp exists bailing out; exit 1; fi
-mkdir /tmp/k8-logs-script-$namespace-$datastamp
+rm -rf "/tmp/k8-logs-script-$namespace-$datastamp"
+if [ -e /tmp/k8-logs-script-"$namespace-$datastamp" ];then echo "/tmp/k8-logs-script-$namespace-$datastamp exists bailing out"; exit 1; fi
+mkdir "/tmp/k8-logs-script-$namespace-$datastamp"
 cd /tmp/k8-logs-script-"$namespace"-"$datastamp" || exit
 
 
 for curr_pod in $running_pods; do
-   kubectl -n $namespace logs $curr_pod --tail $taillines &>${curr_pod}.log
+   kubectl -n "$namespace" logs "$curr_pod" --all-containers=true --tail "$taillines" &>"${curr_pod}.log"
 done
 
 cd "$startdir" || exit
-tar czf k8-logs-script-$namespace-$datastamp.tgz -C /tmp k8-logs-script-$namespace-$datastamp
-echo k8-logs-script-$namespace-$datastamp.tgz 
-rm -rf /tmp/k8-logs-script-$namespace-$datastamp
+tar czf "k8-logs-script-$namespace-$datastamp.tgz" -C /tmp "k8-logs-script-$namespace-$datastamp"
+echo "k8-logs-script-$namespace-$datastamp.tgz" 
+rm -rf "/tmp/k8-logs-script-$namespace-$datastamp"
